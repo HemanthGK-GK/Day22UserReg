f3a407d494a966e898574e97b3af229fd1b34d6b
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-13 17:34:58 +0530

    Hemanth:Validating the Firstname

---
+++ UC1FirstName/CustomException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC1FirstName
+{
+   public class CustomException:Exception
+    {
+        public enum ExceptionsType
+        {
+            Invalid_Data
+        }
+        public ExceptionsType type;
+        public CustomException(ExceptionsType Type, string message) : base(message)
+        {
+            this.type = Type;
+
+        }
+    }
+}
---
+++ UC1FirstName/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UC1FirstName
+{
+   
+        public class ChkFirstName
+        {
+            public string RegexFirstName = "^[A-Z]{1}[a-z]{2,}$";
+            public bool Validate(string firstname)
+            {
+                if (firstname == null)
+                {
+                    throw new CustomException(CustomException.ExceptionsType.Invalid_Data, "Invalid FirstName");
+                }
+                return Regex.IsMatch(firstname, RegexFirstName);
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+        }
+    }
+
